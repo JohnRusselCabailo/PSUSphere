01d4b954a3410ccfd2da51b60fb115d931ea4615
JohnRusselCabailo <202380167@psu.palawan.edu.ph>
2025-09-25 23:34:27 +0800

    fixed homepageviewn

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -9,7 +9,39 @@ from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class OrganizationDeleteView(LoginRequiredMixin, DeleteView):
+class HomePageView(LoginRequiredMixin,ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+
+
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+
+        context["total_org"] = OrgMember.objects.count()
+
+        context["total_program"] = Program.objects.count()
+
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+
+        context["students_joined_this_year"] = count
+
+        
+
+        return context
+
+
+class OrganizationDeleteView(DeleteView):
     model = Organization
     template_name = 'org_del.html'
     success_url = reverse_lazy('organization-list')
@@ -67,36 +99,6 @@ class OrganizationUpdateView(UpdateView):
     template_name = 'org_form.html'
     success_url = reverse_lazy('organization-list')
 
-class HomePageView(ListView):
-    model = Organization
-    context_object_name = 'home'
-    template_name = "home.html"
-
-    def get_context_data(self, **kwargs):
-
-
-        context = super().get_context_data(**kwargs)
-        context["total_students"] = Student.objects.count()
-
-        context["total_org"] = OrgMember.objects.count()
-
-        context["total_program"] = Program.objects.count()
-
-        today = timezone.now().date()
-        count = (
-            OrgMember.objects.filter(
-                date_joined__year=today.year
-            )
-            .values("student")
-            .distinct()
-            .count()
-        )
-
-        context["students_joined_this_year"] = count
-
-        
-
-        return context
 
 
 class OrganizationList(ListView):
